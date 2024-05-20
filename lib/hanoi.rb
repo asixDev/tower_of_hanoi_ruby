56a9cdf6102d9e9adee1775915aab4f95b1504eb
asixDev <ignacy.jarzemski@gmail.com>
2024-05-20 11:01:24 +0200

    init

---
+++ lib/hanoi.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hanoi/version"
+
+module Hanoi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hanoi/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Hanoi
+  VERSION = "0.1.0"
+end
